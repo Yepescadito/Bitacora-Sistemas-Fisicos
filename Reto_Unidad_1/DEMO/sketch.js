a35f1407a0a886cd9789241519b5959b6c96ccd4
Yepes <B09S314est@MEDAULAS.UPB>
2026-07-22 17:20:13 -0500

    Init

---
+++ Reto_Unidad_1/DEMO/sketch.js
@@ -0,0 +1,35 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+let walker;
+
+function setup() {
+  createCanvas(640, 240);
+  walker = new Walker();
+  background(255);
+}
+
+function draw() {
+  walker.step();
+  walker.show();
+}
+
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+
+  show() {
+    stroke("blue");
+    fill(0, 10);
+    circle(this.x, 120, 16);
+    //circle(this.x, this.y, 25);
+    //point(this.x, this.y);
+  }
+
+  step() {
+    this.x = randomGaussian(width / 2, 60);
+  }
+}
