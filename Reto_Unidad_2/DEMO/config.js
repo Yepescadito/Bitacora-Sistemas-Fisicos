7105da11eba4e8c4e295499856207b8b1980a5ee
Yepes <alejabmx202@gmail.com>
2026-08-04 22:28:37 -0500

    Avance Pagina

--- Reto_Unidad_2/DEMO/config.js
+++ Reto_Unidad_2/DEMO/config.js
@@ -4,7 +4,7 @@ const Config = {
     { name: 'Santa Fe', color: [255, 60, 60], mass: 0.95 },
     { name: 'Bucaramanga', color: [255, 225, 65], mass: 1.05 },
     { name: 'Millonarios', color: [80, 145, 255], mass: 1.0 },
-    { name: 'Envigado', color: [255, 145, 60], mass: 0.98 }
+    { name: 'La Equidad', color: [255, 255, 255], mass: 0.98 }
   ],
   defaultSettings: {
     interactionRadius: 130,
